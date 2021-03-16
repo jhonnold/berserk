af043b124c0e0be815adadb5affe91958f26b41e
Jay Honnold <jayhonnold@gmail.com>
2021-03-15 19:32:55 -0500

    Decrease time more to prevent timeouts

--- src/uci.c
+++ src/uci.c
@@ -70,7 +70,7 @@ void parseGo(char* in, SearchParams* params, Board* board) {
     if (time != -1) {
       params->timeset = 1;
       time /= movesToGo;
-      time -= 15;
+      time -= 30;
       time += inc;
       params->endTime = params->startTime + time;
     } else {
