351838cb04454a9163f421efa801d0e53cdd3e11
Jay Honnold <jayhonnold@gmail.com>
2021-03-21 14:16:11 -0500

    KISS PRNG (Cheng author)
    Bench: 9534328

--- src/random.c
+++ src/random.c
@@ -1,26 +1,26 @@
+#include <stdint.h>
+
 #include "random.h"
 
-uint32_t randomState = INITIAL_RANDOM_STATE;
+// I dunno anything about random number generators, and had a bad one for a while
+// Thanks to Martin Sedlák (author of Cheng) this one is really cool and works :)
+// http://www.vlasak.biz/cheng/
 
-uint32_t randomInt() {
-  uint32_t result = randomState;
+uint64_t keys[2];
 
-  result ^= (result << 13);
-  result ^= (result >> 17);
-  result ^= (result << 5);
+inline uint64_t rotate(uint64_t v, uint8_t s) { return (v >> s) | (v << (64 - s)); }
 
-  randomState = result;
-
-  return result;
+inline uint64_t randomLong() {
+  uint64_t tmp = keys[0];
+  keys[0] += rotate(keys[1] ^ 0xc5462216u ^ ((uint64_t)0xcf14f4ebu << 32), 1);
+  return keys[1] += rotate(tmp ^ 0x75ecfc58u ^ ((uint64_t)0x9576080cu << 32), 9);
 }
 
-uint64_t randomLong() {
-  uint64_t n1 = randomInt() & 0xFFFF;
-  uint64_t n2 = randomInt() & 0xFFFF;
-  uint64_t n3 = randomInt() & 0xFFFF;
-  uint64_t n4 = randomInt() & 0xFFFF;
+void seedRandom(uint64_t seed) {
+  keys[0] = keys[1] = seed;
 
-  return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
+  for (int i = 0; i < 64; i++)
+    randomLong();
 }
 
 uint64_t randomMagic() { return randomLong() & randomLong() & randomLong(); }
