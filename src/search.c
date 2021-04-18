ffc36d88599df1b5fde509163ac7905ec98f86fd
Jay Honnold <jayhonnold@gmail.com>
2021-04-18 13:37:23 -0500

    IID fix
    Bench: 7319797

--- src/search.c
+++ src/search.c
@@ -257,6 +257,14 @@ int Negamax(int alpha, int beta, int depth, ThreadData* thread, PV* pv) {
     }
   }
 
+  // iid (internal iterative deepening)
+  // if this is a pv node with no hash entry, then we need to find a good first move
+  // Calling a shallower search will force a tt entry and we probe that once done
+  if (isPV && !ttValue && !isRoot && depth > 5) {
+    Negamax(alpha, beta, depth - 2, thread, pv);
+    ttValue = TTProbe(board->zobrist);
+  }
+
   MoveList moveList;
   GenerateAllMoves(&moveList, board, data);
 
@@ -280,14 +288,6 @@ int Negamax(int alpha, int beta, int depth, ThreadData* thread, PV* pv) {
       // quiet moves use a quadratic scale upwards
       if (SEE(board, move) < STATIC_PRUNE[tactical][depth])
         continue;
-    }
-
-    // iid (internal iterative deepening)
-    // if this is a pv node with no hash entry, then we need to find a good first move
-    // Calling a shallower search will force a tt entry and we probe that once done
-    if (isPV && !ttValue && !isRoot && depth > 5 && data->moves[data->ply - 1] != NULL_MOVE) {
-      Negamax(alpha, beta, depth - 2, thread, pv);
-      ttValue = TTProbe(board->zobrist);
     }
 
     // singular extension
