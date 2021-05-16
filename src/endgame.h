3faf8a8bd215eae203a552c6d8cb1e7c2a00df94
Jay Honnold <jayhonnold@gmail.com>
2021-05-16 11:49:03 -0700

    KPK

--- src/endgame.h
+++ src/endgame.h
@@ -24,8 +24,8 @@ int StaticMaterialScore(int side, Board* board);
 int EvaluateMaterialOnlyEndgame(Board* board);
 int EvaluateKXK(Board* board);
 
-uint8_t GetKPKBit(int bit);
-int KPKIndex(int ssKing, int wsKing, int p, int stm);
-int KPKDraw(int ss, int ssKing, int wsKing, int p, int stm);
+uint8_t GetKPKBit(uint32_t bit);
+uint32_t KPKIndex(int ssKing, int wsKing, int p, int stm);
+uint8_t KPKDraw(int ss, int ssKing, int wsKing, int p, int stm);
 
 #endif
