941e615c52a573918a6b43d4b58946e262af48d3
Jay Honnold <jayhonnold@gmail.com>
2021-07-06 22:10:06 -0500

    Move Pruning Cleanup
    Bench: 9746231

--- src/search.c
+++ src/search.c
@@ -387,22 +387,21 @@ int Negamax(int alpha, int beta, int depth, ThreadData* thread, PV* pv) {
     int specialQuiet = !tactical && (move == moves.killer1 || move == moves.killer2 || move == moves.counter);
     int hist = !tactical ? GetHistory(data, move, board->side) : 0;
     int counterHist = !tactical ? GetCounterHistory(data, move) : 0;
+    int see = 0;
 
-    if (bestScore > -MATE_BOUND && depth <= 8 && totalMoves >= LMP[improving][depth])
-      skipQuiets = 1;
+    if (bestScore > -MATE_BOUND) {
+      if (depth <= 8 && totalMoves >= LMP[improving][depth])
+        skipQuiets = 1;
 
-    // counter move history pruning
-    if (bestScore > -MATE_BOUND && !tactical && !specialQuiet && depth <= 2 && counterHist <= -8192)
-      continue;
+      if (!tactical && !specialQuiet && depth < 3 && counterHist <= -8192)
+        continue;
 
-    // Static evaluation pruning, this applies for both quiet and tactical moves
-    // quiet moves use a quadratic scale upwards
-    if (bestScore > -MATE_BOUND && tactical && moves.phase > PLAY_GOOD_TACTICAL &&
-        SEE(board, move) < STATIC_PRUNE[1][depth])
-      continue;
+      if (tactical && moves.phase > PLAY_GOOD_TACTICAL && SEE(board, move) < STATIC_PRUNE[1][depth])
+        continue;
 
-    if (bestScore > -MATE_BOUND && !tactical && SEE(board, move) < STATIC_PRUNE[0][depth])
-      continue;
+      if (!tactical && SEE(board, move) < STATIC_PRUNE[0][depth])
+        continue;
+    }
 
     nonPrunedMoves++;
 
