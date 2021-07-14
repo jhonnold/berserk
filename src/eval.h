46d543ae80463c48a7fb6541b90a08211aaf48cd
Jay Honnold <jhonnold@users.noreply.github.com>
2021-07-14 15:09:28 -0500

    Rook to Open (#220)
    Bench: 7899386
    
    ELO | 2.32 +- 2.40 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=16MB
    LLR | 2.96 (-2.94, 2.94) [-1.00, 4.00]
    GAMES | N: 39416 W: 9840 L: 9577 D: 19999

--- src/eval.h
+++ src/eval.h
@@ -63,6 +63,7 @@ extern const Score BAD_BISHOP_PAWNS;
 extern const Score DRAGON_BISHOP;
 extern const Score ROOK_OPEN_FILE;
 extern const Score ROOK_SEMI_OPEN;
+extern const Score ROOK_TO_OPEN;
 extern const Score QUEEN_OPPOSITE_ROOK;
 extern const Score QUEEN_ROOK_BATTERY;
 
