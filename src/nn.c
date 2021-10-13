0ea2aa966e6fa33f0dcbf784ad80e9e2b25bf03e
Jay Honnold <jayhonnold@gmail.com>
2021-10-13 11:15:34 -0700

    Half P

--- src/nn.c
+++ src/nn.c
@@ -67,19 +67,23 @@ void ApplyFirstLayer(Board* board, Accumulator output, int perspective) {
   }
 }
 
-#if defined(__AVX_2_)
-int ApplySecondLayer(Accumulator hidden) {
+#if defined(__AVX__)
+int ApplySecondLayer(Accumulator a1, Accumulator a2) {
   int result = OUTPUT_BIAS * QUANTIZATION_PRECISION_IN;
 
   const __m256i zero = _mm256_setzero_si256();
-  __m256i r8 = _mm256_setzero_si256();
+  __m256i s0 = _mm256_setzero_si256();
+  __m256i s1 = _mm256_setzero_si256();
 
   for (size_t j = 0; j < N_HIDDEN / 16; j++) {
-    const __m256i activated = _mm256_max_epi16(((__m256i*)hidden)[j], zero);
+    const __m256i ac0 = _mm256_max_epi16(((__m256i*)a1)[j], zero);
+    const __m256i ac1 = _mm256_max_epi16(((__m256i*)a2)[j], zero);
 
-    r8 = _mm256_add_epi32(r8, _mm256_madd_epi16(activated, ((__m256i*)HIDDEN_WEIGHTS)[j]));
+    s0 = _mm256_add_epi32(s0, _mm256_madd_epi16(ac0, ((__m256i*)HIDDEN_WEIGHTS)[j]));
+    s1 = _mm256_add_epi32(s1, _mm256_madd_epi16(ac1, ((__m256i*)HIDDEN_WEIGHTS)[j + N_HIDDEN / 16]));
   }
 
+  const __m256i r8 = _mm256_add_epi32(s0, s1);
   const __m128i r4 = _mm_add_epi32(_mm256_castsi256_si128(r8), _mm256_extractf128_si256(r8, 1));
   const __m128i r2 = _mm_add_epi32(r4, _mm_srli_si128(r4, 8));
   const __m128i r1 = _mm_add_epi32(r2, _mm_srli_si128(r2, 4));
@@ -87,19 +91,23 @@ int ApplySecondLayer(Accumulator hidden) {
   result += _mm_cvtsi128_si32(r1);
   return result / QUANTIZATION_PRECISION_IN / QUANTIZATION_PRECISION_OUT;
 }
-#elif defined(__SSE_2_)
-int ApplySecondLayer(Accumulator hidden) {
+#elif defined(__SSE__)
+int ApplySecondLayer(Accumulator a1, Accumulator a2) {
   int result = OUTPUT_BIAS * QUANTIZATION_PRECISION_IN;
 
   const __m128i zero = _mm_setzero_si128();
-  __m128i r4 = _mm_setzero_si128();
+  __m128i s0 = _mm_setzero_si128();
+  __m128i s1 = _mm_setzero_si128();
 
   for (size_t j = 0; j < N_HIDDEN / 8; j++) {
-    const __m128i activated = _mm_max_epi16(((__m128i*)hidden)[j], zero);
+    const __m128i ac0 = _mm_max_epi16(((__m128i*)a1)[j], zero);
+    const __m128i ac1 = _mm_max_epi16(((__m128i*)a2)[j], zero);
 
-    r4 = _mm_add_epi32(r4, _mm_madd_epi16(activated, ((__m128i*)HIDDEN_WEIGHTS)[j]));
+    s0 = _mm_add_epi32(s0, _mm_madd_epi16(ac0, ((__m128i*)HIDDEN_WEIGHTS)[j]));
+    s1 = _mm_add_epi32(s1, _mm_madd_epi16(ac1, ((__m128i*)HIDDEN_WEIGHTS)[j + N_HIDDEN / 8]));
   }
 
+  const __m128i r4 = _mm_add_epi32(s0, s1);
   const __m128i r2 = _mm_add_epi32(r4, _mm_srli_si128(r4, 8));
   const __m128i r1 = _mm_add_epi32(r2, _mm_srli_si128(r2, 4));
 
