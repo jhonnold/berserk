8cbbc09d8dec1d498862974d52f6d082041103f6
Jay Honnold <jayhonnold@gmail.com>
2021-10-29 22:26:23 -0700

    Bench: 4090877

--- src/search.c
+++ src/search.c
@@ -560,6 +560,9 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
         if (PIECE_TYPE[MovePiece(move)] == PAWN_TYPE && rank(MoveEnd(move) ^ (board->xside == WHITE ? 0 : 56)) == 1)
           R--;
 
+        if (board->checkers) // move GAVE check
+          R--;
+
         if (MoveCapture(nullThreat) && MoveStart(move) != MoveEnd(nullThreat) && !board->checkers)
           R++;
 
