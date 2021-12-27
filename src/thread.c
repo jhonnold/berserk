2ad160c477f4d6656b85b578ee238056c7f56f2c
Jay Honnold <jhonnold@users.noreply.github.com>
2021-12-26 18:41:24 -0700

    Offset moves by 2 (#317)
    Bench: 3206799
    
    This changes bench as a NULL_MOVE is seen the same as a pawn promoting on a8. This is rare and should have little effect.
    
    ELO | 5.46 +- 5.85 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=8MB
    LLR | 2.98 (-2.94, 2.94) [-5.00, 0.00]
    GAMES | N: 6552 W: 1639 L: 1536 D: 3377

--- src/thread.c
+++ src/thread.c
@@ -63,8 +63,11 @@ void InitPool(Board* board, SearchParams* params, ThreadData* threads, SearchRes
     // empty unneeded data
     memset(&threads[i].data.skipMove, 0, sizeof(threads[i].data.skipMove));
     memset(&threads[i].data.evals, 0, sizeof(threads[i].data.evals));
-    memset(&threads[i].data.moves, 0, sizeof(threads[i].data.moves));
-    
+
+    // set the moves arr as an offset of 2
+    memset(&threads[i].data.searchMoves, 0, sizeof(threads[i].data.searchMoves));
+    threads[i].data.moves = &threads[i].data.searchMoves[2];
+
     memset(&threads[i].scores, 0, sizeof(threads[i].scores));
     memset(&threads[i].bestMoves, 0, sizeof(threads[i].bestMoves));
     memset(&threads[i].pvs, 0, sizeof(threads[i].pvs));
