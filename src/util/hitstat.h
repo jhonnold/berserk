4ecabdd0c1e43fafd01a87e18d32b062a1cd56a7
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-04-27 10:11:23 +0300

    updato 'Reduce loop overhead'

--- src/util/hitstat.h
+++ src/util/hitstat.h
@@ -5,7 +5,10 @@
 static uint64_t hitmap[MAX_HITCOUNT][2]={0};
 static long double meansum[MAX_HITCOUNT]={0};
 static uint64_t hitsum[MAX_HITCOUNT]={0};
-#define mean(counter,num) (meansum[counter]+=num,hitsum[counter]++)
-#define hit(counter,cond) (hitmap[counter][0]++,hitmap[counter][1]+=!!(cond))
-#define printhits()  for(size_t counter=0;counter<MAX_HITCOUNT;counter++){if(hitmap[counter][0]){print("\nHCounter:",counter," Total:",hitmap[counter][0]," Hits:",hitmap[counter][1],"Hit%:",hitmap[counter][1]*100.0/hitmap[counter][0]);} }puts("");
-#define printmeans() for(size_t counter=0;counter<MAX_HITCOUNT;counter++){if(hitsum[counter]){print("\nMCounter:",counter," Total:",hitsum[counter]," Average:",meansum[counter]/hitsum[counter]);} }puts("");
+static uint8_t meanset_flag=0,hitset_flag=0;
+
+#define mean(counter,num) (meansum[counter]+=num,hitsum[counter]++,meanset_flag=1)
+#define hit(counter,cond) (hitmap[counter][0]++,hitmap[counter][1]+=!!(cond),hitset_flag=1)
+#define printhits() if(hitset_flag){ for(size_t counter=0;counter<MAX_HITCOUNT;counter++){if(hitmap[counter][0]){print("\nHCounter:",counter," Total:",hitmap[counter][0]," Hits:",hitmap[counter][1],"Hit%:",hitmap[counter][1]*100.0/hitmap[counter][0]);} }puts("");}
+
+#define printmeans() if(meanset_flag){ for(size_t counter=0;counter<MAX_HITCOUNT;counter++){if(hitsum[counter]){print("\nMCounter:",counter," Total:",hitsum[counter]," Average:",meansum[counter]/hitsum[counter]);} }puts("");}
