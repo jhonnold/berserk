8d3353ccba34855f2c79623d16fc278564828744
Jay Honnold <jayhonnold@gmail.com>
2022-05-16 19:10:55 -0700

    Bench: 3474469

--- src/types.h
+++ src/types.h
@@ -115,8 +115,7 @@ typedef struct {
   Move killers[MAX_SEARCH_PLY][2];  // killer moves, 2 per ply
   Move counters[64 * 64];           // counter move butterfly table
   int hh[2][65][64 * 64];           // history heuristic butterfly table (stm)
-  int ch[6][64][6][64];             // counter move history table
-  int fh[6][64][6][64];             // follow up history table
+  int ch[12][64][12][64];           // continuation move history table
 
   int th[6][64][6];  // tactical (capture) history
 
