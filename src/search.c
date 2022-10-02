a3f4ea846d4307e205e29ce00ce89b91da1222bf
Jay Honnold <jayhonnold@gmail.com>
2022-10-02 14:35:17 -0700

    Bench: 4941654

--- src/search.c
+++ src/search.c
@@ -49,7 +49,7 @@ extern volatile int PONDERING;
 
 void InitPruningAndReductionTables() {
   for (int depth = 1; depth < MAX_SEARCH_PLY; depth++)
-    for (int moves = 1; moves < 64; moves++) LMR[depth][moves] = log(depth) * log(moves) / 2 - 0.2;
+    for (int moves = 1; moves < 64; moves++) LMR[depth][moves] = log(depth) * log(moves) / 2.25 + 0.25;
 
   LMR[0][0] = LMR[0][1] = LMR[1][0] = 0;
 
