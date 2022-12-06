74ec5e889dfe1ec60ac8400a31596043e4175748
Jay Honnold <jayhonnold@gmail.com>
2022-12-06 07:53:51 -0700

    Bench: 4561441

--- src/search.c
+++ src/search.c
@@ -472,7 +472,7 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
   Move quiets[64];
   Move tacticals[64];
 
-  int legalMoves = 0, playedMoves = 0, numQuiets = 0, numTacticals = 0, skipQuiets = 0;
+  int legalMoves = 0, playedMoves = 0, numQuiets = 0, numTacticals = 0, skipQuiets = 0, singularExtension = 0;
   InitAllMoves(&moves, hashMove, data, oppThreat.sqs);
 
   while ((move = NextMove(&moves, board, skipQuiets))) {
@@ -533,6 +533,8 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
 
       // no score failed above sBeta, so this is singular
       if (score < sBeta) {
+        singularExtension = 1;
+
         if (!isPV && score < sBeta - 35 && data->de[data->ply - 1] <= 6) {
           extension           = 2;
           data->de[data->ply] = data->de[data->ply - 1] + 1;
@@ -591,7 +593,7 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
       R -= history / 20480;
 
       // prevent dropping into QS, extending, or reducing all extensions
-      R = min(depth - 1, max(R, 1));
+      R = min(depth - 1, max(R, !singularExtension));
     }
 
     // First move of a PV node
