e23c1454f114a6fb2020595ff4492307d36062aa
Jay Honnold <jayhonnold@gmail.com>
2023-01-15 06:57:56 -0700

    Happy New Year
    Bench: 5272615

--- src/eval.h
+++ src/eval.h
@@ -1,5 +1,5 @@
 // Berserk is a UCI compliant chess engine written in C
-// Copyright (C) 2022 Jay Honnold
+// Copyright (C) 2023 Jay Honnold
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
