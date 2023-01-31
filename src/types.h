9e0ca75f162fcac351105c3d48f28d2c3abf75a0
Jay Honnold <jayhonnold@gmail.com>
2023-01-31 16:39:00 -0700

    (2x768)->8->32

--- src/types.h
+++ src/types.h
@@ -28,7 +28,7 @@
 #define N_KING_BUCKETS 16
 
 #define N_FEATURES (N_KING_BUCKETS * 12 * 64)
-#define N_HIDDEN   512
+#define N_HIDDEN   768
 #define N_L1       (2 * N_HIDDEN)
 #define N_L2       8
 #define N_L3       32
