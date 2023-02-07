58fc09e992c8a2a9a7a9a0608f51e01ad45bd70b
Jay Honnold <jhonnold@users.noreply.github.com>
2023-02-06 18:50:02 -0700

    Updates (#460)
    Bench: 4824204
    
    These are changes I want, so I've decided to merge them, even at a serious loss of probably 10-15 Elo.

--- src/types.h
+++ src/types.h
@@ -210,6 +210,11 @@ enum {
   // QSearch
   QS_GEN_NOISY_MOVES,
   QS_PLAY_NOISY_MOVES,
+  // QSearch Evasions
+  QS_EVASION_HASH_MOVE,
+  QS_GEN_EVASIONS,
+  QS_PLAY_EVASIONS,
+  // ...
   NO_MORE_MOVES,
   PERFT_MOVES,
 };
