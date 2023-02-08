258eb628fe7246da0c262e4e226e0983a8e417dc
Jay Honnold <jayhonnold@gmail.com>
2023-02-08 12:46:55 -0700

    Fix AVX512

--- src/accumulator.h
+++ src/accumulator.h
@@ -19,10 +19,9 @@
 
 #include "types.h"
 
-#define NUM_REGS 16
-
 #if defined(__AVX512F__)
-#define UNROLL     512
+#define UNROLL     256
+#define NUM_REGS   8
 #define regi_t     __m512i
 #define regi_load  _mm512_load_si512
 #define regi_sub   _mm512_sub_epi16
@@ -30,6 +29,7 @@
 #define regi_store _mm512_store_si512
 #elif defined(__AVX2__)
 #define UNROLL     256
+#define NUM_REGS   16
 #define regi_t     __m256i
 #define regi_load  _mm256_load_si256
 #define regi_sub   _mm256_sub_epi16
@@ -37,6 +37,7 @@
 #define regi_store _mm256_store_si256
 #else
 #define UNROLL     128
+#define UNROLL     16
 #define regi_t     __m128i
 #define regi_load  _mm_load_si128
 #define regi_sub   _mm_sub_epi16
