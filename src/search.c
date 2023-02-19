f783f92ec5a3d63a52e5e41730da73cf6a56db96
Jay Honnold <jhonnold@users.noreply.github.com>
2023-02-19 00:41:44 -0700

    Tweak history reductions in LMR (#468)
    Bench: 4309157
    
    STC
    
    ELO   | 0.78 +- 2.29 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=8MB
    LLR   | -0.45 (-2.94, 2.94) [0.00, 3.00]
    GAMES | N: 40680 W: 9410 L: 9319 D: 21951
    
    LTC
    
    ELO   | 2.51 +- 1.99 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 3.00]
    GAMES | N: 51072 W: 11326 L: 10957 D: 28789

--- src/search.c
+++ src/search.c
@@ -642,7 +642,7 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
         R += 1 + !IsCap(move);
 
       // adjust reduction based on historical score
-      R -= history / 6144;
+      R -= history / 8192;
 
       // prevent dropping into QS, extending, or reducing all extensions
       R = Min(depth - 1, Max(R, !singularExtension));
