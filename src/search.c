04250428a20b20d21ecbd512bd4e296c0d53c346
Jay Honnold <jayhonnold@gmail.com>
2023-08-12 19:00:06 -0700

    Bench: 5062869

--- src/search.c
+++ src/search.c
@@ -460,12 +460,12 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
   if (!isPV && !inCheck) {
     // Reverse Futility Pruning
     // i.e. the static eval is so far above beta we prune
-    if (depth <= 9 && !ss->skip && eval < WINNING_ENDGAME &&
-        eval - 75 * depth + 100 * (improving && !oppThreatPcs) >= beta && eval >= beta)
+    if (depth <= 8 && !ss->skip && eval < WINNING_ENDGAME &&
+        eval - 65 * depth + 117 * (improving && !oppThreatPcs) >= beta && eval >= beta)
       return eval;
 
     // Razoring
-    if (depth <= 5 && eval + 200 * depth <= alpha) {
+    if (depth <= 4 && eval + 139 * depth <= alpha) {
       score = Quiesce(alpha, beta, thread, ss);
       if (score <= alpha)
         return score;
@@ -475,10 +475,10 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
     // i.e. Our position is so good we can give our opponnent a free move and
     // they still can't catch up (this is usually countered by captures or mate
     // threats)
-    if (depth >= 3 && (ss - 1)->move != NULL_MOVE && !ss->skip && eval >= beta &&
+    if (depth >= 5 && (ss - 1)->move != NULL_MOVE && !ss->skip && eval >= beta &&
         // weiss conditional
         HasNonPawn(board) > (depth > 12)) {
-      int R = 4 + depth / 6 + Min((eval - beta) / 256, 3) + !oppThreatPcs;
+      int R = 4 + 438 * depth / 2048 + Min(3 * (eval - beta) / 2048, 6) + !oppThreatPcs;
       R     = Min(depth, R); // don't go too low
 
       TTPrefetch(KeyAfter(board, NULL_MOVE));
@@ -498,8 +498,8 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
     // If a relatively deep search from our TT doesn't say this node is
     // less than beta + margin, then we run a shallow search to look
     Threats(&ss->ownThreat, board, board->stm);
-    int probBeta = beta + 110 - 30 * improving;
-    if (depth > 4 && abs(beta) < TB_WIN_BOUND && ss->ownThreat.pcs &&
+    int probBeta = beta + 103 - 38 * improving;
+    if (depth > 3 && abs(beta) < TB_WIN_BOUND && ss->ownThreat.pcs &&
         !(tt && TTDepth(tt) >= depth - 3 && ttScore < probBeta)) {
       InitPCMovePicker(&mp, thread);
       while ((move = NextMove(&mp, board, 1))) {
