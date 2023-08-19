1be4ae6942e4028d3d72eba0a0f0abf2649dcdb6
Jay Honnold <jayhonnold@gmail.com>
2023-08-18 19:13:08 -0700

    Bench: 5801989

--- src/board.h
+++ src/board.h
@@ -90,7 +90,7 @@ INLINE uint64_t FMRKey(uint64_t seed) {
 }
 
 INLINE uint64_t Key(Board* board) {
-  return board->fmr < 10 ? board->zobrist : board->zobrist ^ FMRKey((board->fmr - 20) / 10);
+  return board->fmr < 5 ? board->zobrist : board->zobrist ^ FMRKey((board->fmr - 5) / 8);
 }
 
 #endif
