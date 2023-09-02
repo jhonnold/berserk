0beeac07504ecec5b8ce2788c586569f1e0037eb
Jay Honnold <jayhonnold@gmail.com>
2023-09-02 13:47:47 -0700

    Bench: 4297135

--- src/movepick.c
+++ src/movepick.c
@@ -50,14 +50,23 @@ void ScoreMoves(MovePicker* picker, Board* board, const int type) {
   while (current < picker->end) {
     Move move = current->move;
 
-    if (type == ST_QUIET)
+    if (type == ST_QUIET) {
       current->score = 2 * ((int) HH(thread->board.stm, move, ss->oppThreat.sqs) + //
                             (int) (*(ss - 1)->ch)[Moving(move)][To(move)] +        //
                             (int) (*(ss - 2)->ch)[Moving(move)][To(move)]) +       //
                        (int) (*(ss - 4)->ch)[Moving(move)][To(move)] +             //
                        (int) (*(ss - 6)->ch)[Moving(move)][To(move)];
 
-    else if (type == ST_CAPTURE)
+      const int pt = PieceType(Moving(move));
+
+      // Clovers king ring mp bonus
+      if (pt != PAWN && pt != KING) {
+        const BitBoard oppKingRing = GetKingAttacks(LSB(PieceBB(KING, board->xstm)));
+
+        current->score += 4096 * BitCount(oppKingRing & GetPieceAttacks(To(move), OccBB(move), pt));
+      }
+
+    } else if (type == ST_CAPTURE)
       current->score = GetCaptureHistory(picker->thread, move) / 16 + SEE_VALUE[PieceType(board->squares[To(move)])];
 
     else if (type == ST_EVASION) {
@@ -218,7 +227,7 @@ Move NextMove(MovePicker* picker, Board* board, int skipQuiets) {
       // fallthrough
     case QS_GEN_QUIET_CHECKS:
       picker->current = picker->moves;
-      picker->end = AddQuietCheckMoves(picker->current, board);
+      picker->end     = AddQuietCheckMoves(picker->current, board);
 
       picker->phase = QS_PLAY_QUIET_CHECKS;
 
