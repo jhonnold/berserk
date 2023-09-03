6d65c1b0f74fb12c9a63a73766136767c4dbc202
Jay Honnold <jayhonnold@gmail.com>
2023-09-02 19:38:31 -0700

    Bench: 4602421

--- src/search.c
+++ src/search.c
@@ -671,8 +671,8 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
       // adjust reduction based on historical score
       R -= 9 * history / 65536;
 
-      // prevent dropping into QS, extending, or reducing all extensions
-      R = Min(depth - 1, Max(R, 1));
+      // Reduce in qsearch (depth) or just a normal reduction (1)
+      R = Min(depth, Max(R, 1));
 
       score = -Negamax(-alpha - 1, -alpha, newDepth - R, 1, thread, &childPv, ss + 1);
 
