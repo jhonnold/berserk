d7fa8e432bb38b5f11315180bc154b9a63b4a8d5
Jay Honnold <jayhonnold@gmail.com>
2023-09-17 21:06:20 -0700

    Bench: 4165061

--- src/eval.c
+++ src/eval.c
@@ -50,7 +50,7 @@ Score Evaluate(Board* board, ThreadData* thread) {
   if (abs(simpleEval) >= abs(thread->rootSimpleEval)     //
                            + abs(thread->rootScore)      //
                            + 8 * board->fmr * board->fmr //
-                           + PC_VALUES[ROOK] + PC_VALUES[KNIGHT])
+                           + PC_VALUES[ROOK] + 1)
     return simpleEval + (thread->nodes & 0x7) - 3;
 
   Accumulator* acc = board->accumulators;
