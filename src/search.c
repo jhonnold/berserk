1995083748d461e5d16f09b420409b7a65c0caea
Jay Honnold <jayhonnold@gmail.com>
2023-09-23 22:00:09 -0700

    Bench: 4520083

--- src/search.c
+++ src/search.c
@@ -550,7 +550,7 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
         skipQuiets = 1;
 
       if (!IsCap(move) && PieceType(Promo(move)) != QUEEN) {
-        int lmrDepth = Max(1, depth - LMR[Min(depth, 63)][Min(legalMoves, 63)]);
+        int lmrDepth = Max(1, depth - LMR[Min(depth, 63)][Min(legalMoves, 63)] - !ttPv);
 
         if (!killerOrCounter && lmrDepth < 6 && history < -2500 * (depth - 1)) {
           skipQuiets = 1;
