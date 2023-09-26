109c7f3cffd0c19479a791b965297ef70f9879f4
Jay Honnold <jayhonnold@gmail.com>
2023-09-26 06:04:31 -0700

    Bench: 3556179

--- src/uci.c
+++ src/uci.c
@@ -178,8 +178,8 @@ void ParseGo(char* in, Board* board) {
       if (movesToGo == -1) {
         int total = Max(1, time + 50 * inc - MOVE_OVERHEAD);
 
-        Limits.alloc = Min(time * 0.3308, total * 0.0506);
-        Limits.max   = Min((time - MOVE_OVERHEAD) * 0.7624, Limits.alloc * 5.5214);
+        Limits.alloc = Min(time * 0.1983, total * 0.0528);
+        Limits.max   = Min((time - MOVE_OVERHEAD) * 0.7110, Limits.alloc * 5.2118);
       } else {
         int total = Max(1, time + movesToGo * inc - MOVE_OVERHEAD);
 
