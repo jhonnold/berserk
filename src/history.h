1bc590e8939dd6275d83625624cfb384c5d06cc1
Jay Honnold <jayhonnold@gmail.com>
2023-10-27 09:21:10 -0700

    Bench: 2772388

--- src/history.h
+++ src/history.h
@@ -31,7 +31,8 @@ INLINE int GetQuietHistory(SearchStack* ss, ThreadData* thread, Move move) {
   return (int) HH(thread->board.stm, move, thread->board.threatened) + //
          (int) (*(ss - 1)->ch)[Moving(move)][To(move)] +               //
          (int) (*(ss - 2)->ch)[Moving(move)][To(move)] +               //
-         (int) (*(ss - 4)->ch)[Moving(move)][To(move)];
+         (int) (*(ss - 4)->ch)[Moving(move)][To(move)] +               //
+         (int) PH(thread->board.pawnZobrist, move);
 }
 
 INLINE int GetCaptureHistory(ThreadData* thread, Move move) {
