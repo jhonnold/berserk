553f1028c8452ea0e70bd70af6c08fa242fd64f1
Jay Honnold <jayhonnold@gmail.com>
2024-01-25 07:48:24 -0700

    Setup 1280 l1

--- src/nn/accumulator.h
+++ src/nn/accumulator.h
@@ -25,6 +25,7 @@
 #include <immintrin.h>
 #define UNROLL     512
 #define NUM_REGS   16
+#define REG_SIZE   32
 #define regi_t     __m512i
 #define regi_load  _mm512_load_si512
 #define regi_sub   _mm512_sub_epi16
@@ -34,6 +35,7 @@
 #include <immintrin.h>
 #define UNROLL     256
 #define NUM_REGS   16
+#define REG_SIZE   16
 #define regi_t     __m256i
 #define regi_load  _mm256_load_si256
 #define regi_sub   _mm256_sub_epi16
@@ -43,6 +45,7 @@
 #include <immintrin.h>
 #define UNROLL     128
 #define NUM_REGS   16
+#define REG_SIZE   8
 #define regi_t     __m128i
 #define regi_load  _mm_load_si128
 #define regi_sub   _mm_sub_epi16
@@ -51,6 +54,7 @@
 #else
 #define UNROLL           16
 #define NUM_REGS         16
+#define REG_SIZE         1
 #define regi_t           acc_t
 #define regi_load(a)     (*(a))
 #define regi_sub(a, b)   ((a) - (b))
@@ -70,30 +74,33 @@ typedef struct {
 INLINE void ApplyDelta(acc_t* dest, acc_t* src, Delta* delta) {
   regi_t regs[NUM_REGS];
 
-  for (size_t c = 0; c < N_HIDDEN / UNROLL; ++c) {
+  const size_t loops = (N_HIDDEN + UNROLL - 1) / UNROLL;
+
+  for (size_t c = 0; c < loops; ++c) {
     const size_t unrollOffset = c * UNROLL;
+    const size_t numRegs      = Min(NUM_REGS, (N_HIDDEN - unrollOffset) / REG_SIZE);
 
     const regi_t* inputs = (regi_t*) &src[unrollOffset];
     regi_t* outputs      = (regi_t*) &dest[unrollOffset];
 
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_load(&inputs[i]);
 
     for (size_t r = 0; r < delta->r; r++) {
       const size_t offset   = delta->rem[r] * N_HIDDEN + unrollOffset;
       const regi_t* weights = (regi_t*) &INPUT_WEIGHTS[offset];
-      for (size_t i = 0; i < NUM_REGS; i++)
+      for (size_t i = 0; i < numRegs; i++)
         regs[i] = regi_sub(regs[i], weights[i]);
     }
 
     for (size_t a = 0; a < delta->a; a++) {
       const size_t offset   = delta->add[a] * N_HIDDEN + unrollOffset;
       const regi_t* weights = (regi_t*) &INPUT_WEIGHTS[offset];
-      for (size_t i = 0; i < NUM_REGS; i++)
+      for (size_t i = 0; i < numRegs; i++)
         regs[i] = regi_add(regs[i], weights[i]);
     }
 
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regi_store(&outputs[i], regs[i]);
   }
 }
@@ -101,26 +108,29 @@ INLINE void ApplyDelta(acc_t* dest, acc_t* src, Delta* delta) {
 INLINE void ApplySubAdd(acc_t* dest, acc_t* src, int f1, int f2) {
   regi_t regs[NUM_REGS];
 
-  for (size_t c = 0; c < N_HIDDEN / UNROLL; ++c) {
+  const size_t loops = (N_HIDDEN + UNROLL - 1) / UNROLL;
+
+  for (size_t c = 0; c < loops; ++c) {
     const size_t unrollOffset = c * UNROLL;
+    const size_t numRegs      = Min(NUM_REGS, (N_HIDDEN - unrollOffset) / REG_SIZE);
 
     const regi_t* inputs = (regi_t*) &src[unrollOffset];
     regi_t* outputs      = (regi_t*) &dest[unrollOffset];
 
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_load(&inputs[i]);
 
     const size_t o1  = f1 * N_HIDDEN + unrollOffset;
     const regi_t* w1 = (regi_t*) &INPUT_WEIGHTS[o1];
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_sub(regs[i], w1[i]);
 
     const size_t o2  = f2 * N_HIDDEN + unrollOffset;
     const regi_t* w2 = (regi_t*) &INPUT_WEIGHTS[o2];
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_add(regs[i], w2[i]);
 
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regi_store(&outputs[i], regs[i]);
   }
 }
@@ -128,31 +138,34 @@ INLINE void ApplySubAdd(acc_t* dest, acc_t* src, int f1, int f2) {
 INLINE void ApplySubSubAdd(acc_t* dest, acc_t* src, int f1, int f2, int f3) {
   regi_t regs[NUM_REGS];
 
-  for (size_t c = 0; c < N_HIDDEN / UNROLL; ++c) {
+  const size_t loops = (N_HIDDEN + UNROLL - 1) / UNROLL;
+
+  for (size_t c = 0; c < loops; ++c) {
     const size_t unrollOffset = c * UNROLL;
+    const size_t numRegs      = Min(NUM_REGS, (N_HIDDEN - unrollOffset) / REG_SIZE);
 
     const regi_t* inputs = (regi_t*) &src[unrollOffset];
     regi_t* outputs      = (regi_t*) &dest[unrollOffset];
 
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_load(&inputs[i]);
 
     const size_t o1  = f1 * N_HIDDEN + unrollOffset;
     const regi_t* w1 = (regi_t*) &INPUT_WEIGHTS[o1];
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_sub(regs[i], w1[i]);
 
     const size_t o2  = f2 * N_HIDDEN + unrollOffset;
     const regi_t* w2 = (regi_t*) &INPUT_WEIGHTS[o2];
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_sub(regs[i], w2[i]);
 
     const size_t o3  = f3 * N_HIDDEN + unrollOffset;
     const regi_t* w3 = (regi_t*) &INPUT_WEIGHTS[o3];
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_add(regs[i], w3[i]);
 
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regi_store(&outputs[i], regs[i]);
   }
 }
@@ -160,36 +173,39 @@ INLINE void ApplySubSubAdd(acc_t* dest, acc_t* src, int f1, int f2, int f3) {
 INLINE void ApplySubSubAddAdd(acc_t* dest, acc_t* src, int f1, int f2, int f3, int f4) {
   regi_t regs[NUM_REGS];
 
-  for (size_t c = 0; c < N_HIDDEN / UNROLL; ++c) {
+  const size_t loops = (N_HIDDEN + UNROLL - 1) / UNROLL;
+
+  for (size_t c = 0; c < loops; ++c) {
     const size_t unrollOffset = c * UNROLL;
+    const size_t numRegs      = Min(NUM_REGS, (N_HIDDEN - unrollOffset) / REG_SIZE);
 
     const regi_t* inputs = (regi_t*) &src[unrollOffset];
     regi_t* outputs      = (regi_t*) &dest[unrollOffset];
 
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_load(&inputs[i]);
 
     const size_t o1  = f1 * N_HIDDEN + unrollOffset;
     const regi_t* w1 = (regi_t*) &INPUT_WEIGHTS[o1];
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_sub(regs[i], w1[i]);
 
     const size_t o2  = f2 * N_HIDDEN + unrollOffset;
     const regi_t* w2 = (regi_t*) &INPUT_WEIGHTS[o2];
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_sub(regs[i], w2[i]);
 
     const size_t o3  = f3 * N_HIDDEN + unrollOffset;
     const regi_t* w3 = (regi_t*) &INPUT_WEIGHTS[o3];
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_add(regs[i], w3[i]);
 
     const size_t o4  = f4 * N_HIDDEN + unrollOffset;
     const regi_t* w4 = (regi_t*) &INPUT_WEIGHTS[o4];
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regs[i] = regi_add(regs[i], w4[i]);
 
-    for (size_t i = 0; i < NUM_REGS; i++)
+    for (size_t i = 0; i < numRegs; i++)
       regi_store(&outputs[i], regs[i]);
   }
 }
