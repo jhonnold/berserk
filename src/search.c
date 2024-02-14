1bca5ede61c731687b974901cb29694d42703dcb
Jay Honnold <jayhonnold@gmail.com>
2024-02-13 18:43:35 -0700

    Bench: 2588816

--- src/search.c
+++ src/search.c
@@ -44,7 +44,9 @@
 // arrays to store these pruning cutoffs at specific depths
 int LMR[MAX_SEARCH_PLY][64];
 int LMP[2][MAX_SEARCH_PLY];
-int STATIC_PRUNE[2][MAX_SEARCH_PLY];
+
+int QUIET_SEE_PRUNE[MAX_SEARCH_PLY];
+int CAP_SEE_PRUNE[2][MAX_SEARCH_PLY];
 
 void InitPruningAndReductionTables() {
   for (int depth = 1; depth < MAX_SEARCH_PLY; depth++)
@@ -60,8 +62,9 @@ void InitPruningAndReductionTables() {
     LMP[0][depth] = 1.2973 + 0.3772 * depth * depth;
     LMP[1][depth] = 2.7002 + 0.9448 * depth * depth;
 
-    STATIC_PRUNE[0][depth] = -14.9419 * depth * depth; // quiet move cutoff
-    STATIC_PRUNE[1][depth] = -103.9379 * depth;        // capture cutoff
+    QUIET_SEE_PRUNE[depth]  = -14.9419 * depth * depth; // quiet move cutoff
+    CAP_SEE_PRUNE[0][depth] = -103.9379 * depth;        // capture other cutoff
+    CAP_SEE_PRUNE[1][depth] = -66.0 * depth;            // capture cutnode cutoff
   }
 }
 
@@ -632,10 +635,10 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
         if (!inCheck && lmrDepth < 10 && eval + 87 + 46 * lmrDepth <= alpha)
           skipQuiets = 1;
 
-        if (!SEE(board, move, STATIC_PRUNE[0][lmrDepth]))
+        if (!SEE(board, move, QUIET_SEE_PRUNE[lmrDepth]))
           continue;
       } else {
-        if (!SEE(board, move, STATIC_PRUNE[1][depth]))
+        if (!SEE(board, move, CAP_SEE_PRUNE[cutnode][depth]))
           continue;
       }
     }
