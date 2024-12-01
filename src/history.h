5eb129ea3294475f7ac3b805eff9d02b59b5c257
Jay Honnold <jayhonnold@gmail.com>
2024-12-01 16:44:51 -0700

    Bench: 2444014

--- src/history.h
+++ src/history.h
@@ -102,7 +102,7 @@ INLINE int GetEvalCorrection(Board* board, SearchStack* ss, ThreadData* thread) 
   const int nonPawnCorrection = GetNonPawnCorrection(board, thread);
   const int contCorrection = GetContCorrection(ss, thread);
 
-  return pawnCorrection / 2 + nonPawnCorrection + contCorrection;
+  return pawnCorrection / 2 + nonPawnCorrection / 2 + contCorrection;
 }
 
 void UpdateHistories(SearchStack* ss,
