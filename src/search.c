f25b6d3d053e641809b095117c762a4b26d78bf2
Jay Honnold <jayhonnold@gmail.com>
2025-02-05 07:22:33 -0700

    Bench: 2753871

--- src/search.c
+++ src/search.c
@@ -825,6 +825,7 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
   if (!inCheck && !IsCap(bestMove) && (bound & (bestScore >= rawEval ? BOUND_LOWER : BOUND_UPPER))) {
     UpdatePawnCorrection(rawEval, bestScore, depth, board, thread);
     UpdateContCorrection(rawEval, bestScore, depth, ss);
+    UpdateCastleCorrection(rawEval, bestScore, depth, board, thread);
   }
 
   return bestScore;
