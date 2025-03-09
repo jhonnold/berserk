744ec55e46e61cdd9825730edc485ea69fe291f5
Jay Honnold <jayhonnold@gmail.com>
2025-03-09 10:21:39 -0700

    Bench: 2911035

--- src/history.c
+++ src/history.c
@@ -62,13 +62,15 @@ void UpdateHistories(SearchStack* ss,
 
   // Update quiets
   if (!IsCap(bestMove)) {
+    const int malus = -inc + 32 * (nQ - 1);
+
     for (int i = 0; i < nQ; i++) {
       Move m = quiets[i];
       if (m == bestMove)
         continue;
 
-      AddHistoryHeuristic(&HH(stm, m, board->threatened), -inc);
-      UpdateCH(ss, m, -inc);
+      AddHistoryHeuristic(&HH(stm, m, board->threatened), malus);
+      UpdateCH(ss, m, malus);
     }
   }
 
@@ -89,14 +91,14 @@ void UpdateHistories(SearchStack* ss,
 
 void UpdatePawnCorrection(int raw, int real, int depth, Board* board, ThreadData* thread) {
   const int16_t correction = Min(4096, Max(-4096, 4 * (real - raw) * depth));
-  int16_t* pawnCorrection = &thread->pawnCorrection[board->pawnZobrist & PAWN_CORRECTION_MASK];
+  int16_t* pawnCorrection  = &thread->pawnCorrection[board->pawnZobrist & PAWN_CORRECTION_MASK];
   AddHistoryHeuristic(pawnCorrection, correction);
 }
 
 void UpdateContCorrection(int raw, int real, int depth, SearchStack* ss) {
   if ((ss - 1)->move && (ss - 2)->move) {
     const int16_t correction = Min(4096, Max(-4096, 4 * (real - raw) * depth));
-    int16_t* contCorrection = &(*(ss - 2)->cont)[Moving((ss - 1)->move)][To((ss - 1)->move)];
+    int16_t* contCorrection  = &(*(ss - 2)->cont)[Moving((ss - 1)->move)][To((ss - 1)->move)];
     AddHistoryHeuristic(contCorrection, correction);
   }
 }
