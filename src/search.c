e2a9bf974eb7c39c1185fdb4e64d82c63a17de25
Jay Honnold <jayhonnold@gmail.com>
2025-05-12 06:12:53 -0700

    Bench: 2943071

--- src/search.c
+++ src/search.c
@@ -525,6 +525,8 @@ int Negamax(int alpha, int beta, int depth, int cutnode, ThreadData* thread, PV*
 
     // Razoring
     if (depth <= 5 && eval + 214 * depth <= alpha) {
+      DecRlx(thread->nodes);
+
       score = Quiesce(alpha, beta, 0, thread, ss);
       if (score <= alpha)
         return score;
